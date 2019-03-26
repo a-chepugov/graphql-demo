375396192a9cc17f7ec219539b6f6b82a0eb68f1
a.chepugov <oleksii.chepugov@ria.com>
2019-03-26 12:22:02 +0200

    fix(subscribe) city

--- app/Schema/Subscription/city.js
+++ app/Schema/Subscription/city.js
@@ -6,6 +6,9 @@ const socket = require('../socket').default;
 
 module.exports.add = {
 	type: require('../Types/City').default,
+	resolve(source, args, context, info) {
+		return source
+	},
 	subscribe(source, args, context, info) {
 		return socket.asyncIterator('CITY_CREATED');
 	}
