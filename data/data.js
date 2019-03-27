1a00e9bd42e26609b74d4a106ae7f38015a12b29
a.chepugov <oleksii.chepugov@ria.com>
2019-03-27 11:26:09 +0200

    chore(data)

---
+++ data/data.js
@@ -0,0 +1,60 @@
+let qw =
+	{
+		"id": 5, "name": "Pope Patricia", "sex": 0, "birthdate": 80851485077, "city_id": 4,
+		"phones": [{"id": 6, "owner_id": 5, "value": "+347380949187"}, {"id": 7, "owner_id": 5, "value": "+347790430525"}],
+		"ownedPers": [
+			{"id": 1, "owner_id": 5, "__typename": "Fish"}, {"id": 6, "owner_id": 5, "__typename": "Bird"}, {"id": 7, "owner_id": 5, "__typename": "Bird"}],
+		"pets": [
+			{"id": 1, "name": "Арбек", "sex": 1, "ecotype": "nekton"},
+			{"id": 6, "name": "Амор", "color": "purple", "sex": 1, "family": "Falconiformes"},
+			{"id": 7, "name": "Юфта", "color": "purple", "sex": 0, "family": "Falconiformes"}],
+		"friendsIds": [68, 83, 25, 29, 46, 75],
+		"friends": [
+			{
+				"id": 68, "name": "McKinney Harry", "sex": 1, "birthdate": 609133493905, "city_id": 5,
+				"phones": [{"id": 133, "owner_id": 68, "value": "+749511427138"}, {"id": 134, "owner_id": 68, "value": "+749549607631"}, {"id": 135, "owner_id": 68, "value": "+749592175318"}]
+			},
+			{
+				"id": 83, "name": "Cobb Betty", "sex": 0, "birthdate": 531148452398, "city_id": 4,
+				"phones": [{"id": 162, "owner_id": 83, "value": "+348907545872"}, {"id": 163, "owner_id": 83, "value": "+344969722442"}, {"id": 164, "owner_id": 83, "value": "+341282179198"}]
+			},
+			{
+				"id": 25, "name": "Dawson Griselda", "sex": 0, "birthdate": 946483023271, "city_id": 3,
+				"phones": [{"id": 49, "owner_id": 25, "value": "+442017086951"}]
+			},
+			{
+				"id": 29, "name": "Douglas Anthony", "sex": 1, "birthdate": 173515441959, "city_id": 1,
+				"phones": [{"id": 57, "owner_id": 29, "value": "+612702900422"}, {"id": 58, "owner_id": 29, "value": "+612622800733"}]
+			},
+			{
+				"id": 46, "name": "May Anthony", "sex": 1, "birthdate": 943099997253, "city_id": 4,
+				"phones": [{"id": 93, "owner_id": 46, "value": "+347120241644"}, {"id": 94, "owner_id": 46, "value": "+342287376585"}, {"id": 95, "owner_id": 46, "value": "+344086605770"}]
+			},
+			{
+				"id": 75, "name": "Payne John", "sex": 1, "birthdate": 31010405910, "city_id": 4,
+				"phones": [{"id": 149, "owner_id": 75, "value": "+343081947418"}]
+			}],
+		"city": {
+			"id": 4, "name": "Madrid", "code": "+34",
+			"residents": [
+				{
+					"id": 1, "name": "Miles Donna", "sex": 0, "birthdate": 141155637639, "city_id": 4, "phones": [{"id": 3, "owner_id": 1, "value": "+340858853590"}]
+				},
+				{
+					"id": 4, "name": "Maxwell Thomas", "sex": 1, "birthdate": 900742846312, "city_id": 4,
+					"phones": [{"id": 4, "owner_id": 4, "value": "+345580055629"}, {"id": 5, "owner_id": 4, "value": "+342295091284"}, {"id": 153, "owner_id": 4, "value": "+348832479496"}]
+				},
+				{
+					"id": 5, "name": "Pope Patricia", "sex": 0, "birthdate": 80851485077, "city_id": 4,
+					"phones": [{"id": 6, "owner_id": 5, "value": "+347380949187"}, {"id": 7, "owner_id": 5, "value": "+347790430525"}]
+				},
+				{
+					"id": 6, "name": "Byrd Anthony", "sex": 1, "birthdate": 734475919652, "city_id": 4,
+					"phones": [{"id": 8, "owner_id": 6, "value": "+345245417615"}, {"id": 9, "owner_id": 6, "value": "+345281011880"}]
+				},
+				{
+					"id": 11, "name": "Page Donald", "sex": 1, "birthdate": 763258130465, "city_id": 4,
+					"phones": [{"id": 19, "owner_id": 11, "value": "+342616313804"}, {"id": 20, "owner_id": 11, "value": "+341237967560"}, {"id": 21, "owner_id": 11, "value": "+345426867209"}]
+				}]
+		}
+	}
